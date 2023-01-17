6d27d0a33c9d1ef3a9feaeb1221fe6cecc4afae7
MVD\B5PMMMPD <mike.p.doc.71@gmail.com>
2023-01-17 10:30:49 -0600

    added test for plot_count

---
+++ test/summaries/test_plot_count.py
@@ -0,0 +1,25 @@
+import pytest
+import os
+import arcpy
+from arcgis.features import GeoAccessor, GeoSeriesAccessor
+import pandas as pd
+import fmgpy.summaries.forest_calcs
+import test.test_data
+
+# Arrange
+@pytest.fixture
+def fmg_gdb():
+    fmg_gdb = os.path.join(test.test_data.data_folder(), "Pool12.gdb")
+    return fmg_gdb
+
+
+@pytest.fixture
+def fixed_df(fmg_gdb):
+    fixed_fc_path = os.path.join(fmg_gdb, "Fixed")
+    fixed_df = pd.DataFrame.spatial.from_featureclass(fixed_fc_path)
+    return fixed_df
+
+
+def test_plot_count(fixed_df):
+    plots = fmgpy.summaries.forest_calcs.plot_count(fixed_df)
+    assert isinstance(plots, int)
