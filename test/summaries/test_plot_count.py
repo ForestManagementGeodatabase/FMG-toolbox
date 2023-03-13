60c1beff1a01fe42786afe6bb6fe11b402925690
Chris Hawes <chris.hawes9@gmail.com>
2023-03-13 15:15:46 -0500

    updated test data to oracle FMG schema

--- test/summaries/test_plot_count.py
+++ test/summaries/test_plot_count.py
@@ -9,13 +9,13 @@ import test.test_data
 # Arrange
 @pytest.fixture
 def fmg_gdb():
-    fmg_gdb = os.path.join(test.test_data.data_folder(), "Pool12.gdb")
+    fmg_gdb = os.path.join(test.test_data.data_folder(), "FMG_OracleSchema.gdb")
     return fmg_gdb
 
 
 @pytest.fixture
 def fixed_df(fmg_gdb):
-    fixed_fc_path = os.path.join(fmg_gdb, "Fixed")
+    fixed_fc_path = os.path.join(fmg_gdb, "FIXED_PLOTS")
     fixed_df = pd.DataFrame.spatial.from_featureclass(fixed_fc_path)
     return fixed_df
 
