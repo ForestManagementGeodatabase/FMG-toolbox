cff07592204d1f5967af45a458810e60b447624b
Chris Hawes <chris.hawes9@gmail.com>
2023-03-14 14:53:28 -0500

    added intermediate table descriptions

--- fmgpy/summaries/agg_function_testing.py
+++ fmgpy/summaries/agg_function_testing.py
@@ -1,4 +1,12 @@
 def treecount(tr_sp):
+    """Counts trees, excluding no tree records.
+
+    Keyword Arguments:
+    tr_sp -- Input tree species column.
+
+    Details: Function returns a single value and is to be used within a
+    dataframe to create a tree count column (.groupby, .agg).
+    """
     trees = []
     for val in tr_sp:
         if val == 'NoTree':
@@ -15,8 +23,29 @@ def treecount(tr_sp):
             trees.append(val)
     return float(len(trees))
 
-def plotcount(x):
+
+def plotcount(PID):
+    """Counts unique plots, including no tree plots.
+
+    Keyword Arguments:
+    PID -- Input plot ID column.
+
+    Details: Function returns a single value and is to be used within a
+    dataframe to create a plot count column (.groupby, .agg).
+    """
     return float(x.nunique())
+
+
+def create_tree_table(prism_df)
+    # Set TR_DIA to 0 if TR_SP is NoTree or None
+    # Add a tree size class field (Sap, Pole, Saw, Mature, Over Mature)
+    # Add and Calculate BA column (will always be 10, unless NoTree but use equation)
+    # Add and calculate density column (TPA)
+
+def create_plot_table(tree_table)
+    # Jam fixed & age details onto plot
+
+
 
 
 
