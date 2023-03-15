c2eebf33ff46a6d8001ffcd5b3f209f865e827c8
Chris <chris.hawes9@gmail.com>
2023-03-15 07:09:36 -0500

    add console set up file

---
+++ fmgpy/summaries/console_set_up_cch.py
@@ -0,0 +1,30 @@
+### Console Testing Set Up Start
+# Do Some Imports
+import os
+import sys
+import arcpy
+import arcgis
+from arcgis.features import GeoAccessor, GeoSeriesAccessor
+import math
+import pandas as pd
+import numpy as np
+import importlib
+import fmgpy.summaries.forest_calcs as fcalc
+import fmgpy.summaries.agg_function_testing as tcalc
+
+# Define Some input data
+output_gdb = r'C:\LocalProjects\FMG\LocalWorking\FMG_Code_Testing.gdb' # arcpy.GetParameterAsText(0)
+level = 'SID' # arcpy.GetParameterAsText(1)
+age = r'C:\LocalProjects\FMG\FMG-Toolbox\test\data\FMG_OracleSchema.gdb\AGE_PLOTS' # arcpy.GetParameterAsText(2)
+fixed = r'C:\LocalProjects\FMG\FMG-Toolbox\test\data\FMG_OracleSchema.gdb\FIXED_PLOTS' # arcpy.GetParameterAsText(3)
+prism = r'C:\LocalProjects\FMG\FMG-Toolbox\test\data\FMG_OracleSchema.gdb\PRISM_PLOTS' # arcpy.GetParameterAsText(4)
+
+# Reimport statements for dev changes
+# importlib.reload(arcpy)
+# importlib.reload(forest_calcs)
+
+# Create dataframes
+age_df = pd.DataFrame.spatial.from_featureclass(age)
+fixed_df = pd.DataFrame.spatial.from_featureclass(fixed)
+prism_df = pd.DataFrame.spatial.from_featureclass(prism)
+### Console Testing Set Up End
