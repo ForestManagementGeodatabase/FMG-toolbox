abf72b74168fd97887b9175a8bc9738204870ebb
Chris Hawes <chris.hawes9@gmail.com>
2023-09-20 16:30:51 -0500

    start vert comp summary

---
+++ fmgpy/summaries/07_vert_comp_summary.py
@@ -0,0 +1,75 @@
+import os
+import sys
+import arcpy
+import arcgis
+import math
+import pandas as pd
+import numpy as np
+from arcgis.features import GeoAccessor, GeoSeriesAccessor
+import fmgpy.summaries.forest_calcs as fcalc
+
+# Define inputs
+prism_fc = r"C:\LocalProjects\FMG\FMG-toolbox\test\data\FMG_OracleSchema.gdb\PRISM_PLOTS"
+fixed_fc = r"C:\LocalProjects\FMG\FMG-toolbox\test\data\FMG_OracleSchema.gdb\FIXED_PLOTS"
+age_fc = r"C:\LocalProjects\FMG\FMG-toolbox\test\data\FMG_OracleSchema.gdb\AGE_PLOTS"
+out_gdb = r"C:\LocalProjects\FMG\FMG_CODE_TESTING.gdb"
+
+# Import ESRI feature classes as pandas dataframes
+fixed_df = pd.DataFrame.spatial.from_featureclass(fixed_fc)
+age_df = pd.DataFrame.spatial.from_featureclass(age_fc)
+prism_df = pd.DataFrame.spatial.from_featureclass(prism_fc)
+
+# Create base datasets
+plot_table = fcalc.create_plot_table(fixed_df=fixed_df, age_df=age_df)
+tree_table = fcalc.create_tree_table(prism_df=prism_df)
+
+# Allow output overwrite during testing
+arcpy.env.overwriteOutput = True
+
+# Define list of levels
+levels = ['PID', 'SID', 'SITE', 'UNIT', 'COMP', 'POOL']
+
+for level in levels:
+    if level != 'PID':
+        # Run TPA Case with live tree filter to get CNP_TPA, CNP_BA, CNP_QMDBH, MID_TPA, MID_BA, MID_QMDBH
+        vc_live_tpa = fcalc.tpa_ba_qmdbh_level_by_case(tree_table=tree_table,
+                                                       filter_statement=~tree_table.TR_HLTH.isin(["D", "DEAD"]),
+                                                       case_column='VERT_COMP',
+                                                       level=level)
+        vc_live_tpa = vc_live_tpa \
+            .rename(columns={'BA_VERT_COMP_Canopy': 'CNP_BA',
+                             'QM_DBH_VERT_COMP_Canopy': 'CNP_QMDBH',
+                             'TPA_VERT_COMP_Canopy': 'CNP_TPA',
+                             'BA_VERT_COMP_Midstory': 'MID_BA',
+                             'QM_DBH_VERT_COMP_Midstory': 'MID_QMDBH',
+                             'TPA_VERT_COMP_Midstory': 'MID_TPA'}) \
+            .set_index(level)
+
+        # Run TPA Case with dead tree filter to get CNP_D_TPA, CNP_D_BA, CNP_D_QMDBH, MID_D_TPA, MID_D_BA, MID_D_QMDBH
+        vc_dead_tpa = fcalc.tpa_ba_qmdbh_level_by_case(tree_table=tree_table,
+                                                       filter_statement=tree_table.TR_HLTH.isin(["D", "DEAD"]),
+                                                       case_column='VERT_COMP',
+                                                       level=level)
+        vc_dead_tpa = vc_dead_tpa \
+            .rename(columns={'BA_VERT_COMP_Canopy': 'CNP_D_BA',
+                             'QM_DBH_VERT_COMP_Canopy': 'CNP_D_QMDBH',
+                             'TPA_VERT_COMP_Canopy': 'CNP_D_TPA',
+                             'BA_VERT_COMP_Midstory': 'MID_D_BA',
+                             'QM_DBH_VERT_COMP_Midstory': 'MID_D_QMDBH',
+                             'TPA_VERT_COMP_Midstory': 'MID_D_TPA'}) \
+            .set_index(level)
+
+        # Run SP Prev Pct with Canopy (VERT_COMP) filter to get CNP_DOM_SP, CNP_DOM_SP_PCMP
+
+        # Run SP Prev Pct with Midstory (VERT_COMP) filter to get MID_DOM_SP, MID_DOM_SP_PCMP
+
+        # Run SP Prev Pct with Intermediate (TR_CL==I) filter to get INT_DOM_SP, INT_DOM_SP_PCMP
+
+        # Run HLTH Prev Pct with Canopy (VERT_COMP) filter to get CNP_DOM_HLTH, CNP_DOM_HLTH_PCMP
+
+        # Run HLTH Prev Pct with Midstory (VERT_COMP) filter to get MID_DOM_HLTH, MID_DOM_HLTH_PCMP
+
+        # Run HLTH Prev Pct with Intermediate (TR_CL==I) filter to get INT_DOM_HLTH, INT_DOM_HLTH_PCMP
+
+
+
