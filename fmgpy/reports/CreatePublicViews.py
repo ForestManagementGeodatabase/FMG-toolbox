7a88dccc24d95cbff596955e8b54dfb5ba386553
Chris Hawes <chris.hawes9@gmail.com>
2024-08-19 13:13:22 -0500

    debug complete, script tool messaging added

--- fmgpy/reports/CreatePublicViews.py
+++ fmgpy/reports/CreatePublicViews.py
@@ -16,24 +16,26 @@ import pandas as pd
 import fmgpy.summaries.forest_calcs as fcalc
 
 # Define input geodatabase with summary tables parameter
-in_summary_gdb = r"C:\LocalProjects_ProWorkspace\FMG_Testing_20240724\FMG_Testing_20240724.gdb" #arcpy.GetParameterAsText(0)
+arcpy.AddMessage('Starting: Create FMG Public View')
+in_summary_gdb = arcpy.GetParameterAsText(0)
 
 # Define FMG hierarchy levels for output parameters (script tool radio buttons)
-site_view = 'true' #arcpy.GetParameterAsText(1)
-unit_view = 'true' #arcpy.GetParameterAsText(2)
-comp_view = 'true' #arcpy.GetParameterAsText(3)
-pool_view = 'true' #arcpy.GetParameterAsText(4)
+site_view = arcpy.GetParameterAsText(1)
+unit_view = arcpy.GetParameterAsText(2)
+comp_view = arcpy.GetParameterAsText(3)
+pool_view = arcpy.GetParameterAsText(4)
 
 # Define FMG Hierarchy polygon feature class parameters (use validation to turn these on based on output levels
 # selected above. See link for script tool validation code examples.
 # https://pro.arcgis.com/en/pro-app/latest/arcpy/geoprocessing_and_python/customizing-script-tool-behavior.htm
-SITE = r"C:\LocalProjects_ProWorkspace\FMG_Testing_20240724\FMG_Testing_20240724.gdb\SITE" #arcpy.GetParameterAsText(5)
-UNIT = r"C:\LocalProjects_ProWorkspace\FMG_Testing_20240724\FMG_Testing_20240724.gdb\UNIT" #arcpy.GetParameterAsText(6)
-COMP = r"C:\LocalProjects_ProWorkspace\FMG_Testing_20240724\FMG_Testing_20240724.gdb\COMPARTMENT" #arcpy.GetParameterAsText(7)
-POOL = r"C:\LocalProjects_ProWorkspace\FMG_Testing_20240724\FMG_Testing_20240724.gdb\POOL" #arcpy.GetParameterAsText(8)
+SITE = arcpy.GetParameterAsText(5)
+UNIT = arcpy.GetParameterAsText(6)
+COMP = arcpy.GetParameterAsText(7)
+POOL = arcpy.GetParameterAsText(8)
 
 # Define output geodatabase (default to input geodatabase)
-out_view_gdb = r"C:\LocalProjects_ProWorkspace\FMG_Testing_20240724\FMG_Testing_20240724.gdb" #arcpy.GetParameterAsText(9)
+out_view_gdb = arcpy.GetParameterAsText(9)
+arcpy.AddMessage('Input parameters defined')
 
 # From input parameters, build a dictionary to guide work iterations
 # Build list of levels to evaluate
@@ -56,8 +58,13 @@ summary_tables = arcpy.ListTables()
 # Create dict of geometry parameters
 summary_geom = {'SITE': SITE, 'UNIT': UNIT, 'COMP': COMP, 'POOL': POOL}
 
+# Create empty list of output feature classes
+out_feature_classes = []
+arcpy.AddMessage('Level list, workspace, and summary geometries defined')
+
 # Start work loop
 for level in levels:
+    arcpy.AddMessage('Creating public view table for {0}'.format(level))
     level_tables = [i for i in summary_tables if level in i]
     level_tables.sort()
 
@@ -67,9 +74,11 @@ for level in levels:
     df_hlt_sum = pd.DataFrame.spatial.from_table(level_tables[2]).set_index(level)
     df_mst_sum = pd.DataFrame.spatial.from_table(level_tables[4]).set_index(level)
     df_vtc_sum = pd.DataFrame.spatial.from_table(level_tables[7]).set_index(level)
+    arcpy.AddMessage('    Tabular dataframes created')
 
     # make supporting geom df
     df_geometry = pd.DataFrame.spatial.from_featureclass(summary_geom[level]).set_index(level)
+    arcpy.AddMessage('    Spatial dataframe created')
 
     # Create new dfs with just the required columns
     df_age_filt = df_age_sum.filter(items=['AGE_ORIG'])
@@ -83,6 +92,7 @@ for level in levels:
                                            'INT_DOM_SP', 'INT_DOM_SP_PCMP'])
     df_geometry = df_geometry.drop(columns=['LAST_EDITED_DATE', 'LAST_EDITED_USER', 'CREATED_DATE', 'CREATED_USER',
                                             'SE_ANNO_CAD_DATA', 'OBJECTID'])
+    arcpy.AddMessage('    Tabular and spatial dataframes filtered')
 
     # merge component dfs
     df_merged = df_geometry\
@@ -93,6 +103,7 @@ for level in levels:
                      df_vtc_filt],
               how='left')\
         .reset_index()
+    arcpy.AddMessage('    Dataframes merged to create public view')
 
     df_clean = df_merged.round(decimals=2)
 
@@ -101,26 +112,71 @@ for level in levels:
                                                  col_csv='fmgpy/reports/resources/public_view_cols.csv')
     out_df = df_clean.reindex(labels=reindex_cols,
                             axis='columns')
+    arcpy.AddMessage('    Create and reindex output dataframe')
 
     # Handle nan values appropriately
     nan_fill_dict = fcalc.fmg_nan_fill(col_csv='fmgpy/reports/resources/public_view_cols.csv')
     out_df = out_df\
         .fillna(value=nan_fill_dict)\
         .drop(columns=['index'], errors='ignore')
+    arcpy.AddMessage('    Output dataframe nan values filled')
 
     # Enforce ESRI Compatible Dtypes
     dtype_dict = fcalc.fmg_dtype_enforce(col_csv='fmgpy/reports/resources/public_view_cols.csv')
     out_df = out_df.astype(dtype=dtype_dict, copy=False)
+    arcpy.AddMessage('    Output dataframe ESRI-compatible dtypes enforced')
 
     # send back to ESRI-land
     out_fc = os.path.join(out_view_gdb, f"{level}_PublicView")
+    out_feature_classes.append(out_fc)
     out_df.spatial.to_featureclass(location=out_fc,
                                    has_z=False,
                                    has_m=False,
                                    sanitize_columns=False)
+    arcpy.AddMessage('    Output dataframe exported to {0{'.format(out_fc))
 
-    # Make pretty ESRI land data
+# Make pretty ESRI land data
+# Create dict from CSV
+# import the column definition csv
+alias_csv = pd.read_csv('fmgpy/reports/resources/public_view_cols.csv')
+arcpy.AddMessage('Setting field aliases for exported public views')
+# Create dictionary for field name: field alias
+alias_dict = None
+if len(alias_csv.index) > 0:
+    alias_keys = alias_csv['COL_NAME'].values.tolist()
+    alias_vals = alias_csv['ALIAS'].values.tolist()
+    alias_dict = dict(zip(alias_keys, alias_vals))
+arcpy.AddMessage('Alias dictionary created')
 
+# Iterate through output feature classes, setting field name alias
+for feature_class in out_feature_classes:
+    arcpy.AddMessage('Setting aliases on {0}'.format(feature_class))
+    for field, alias in alias_dict.items():
+        arcpy.management.AlterField(in_table=feature_class,
+                                    field=field,
+                                    new_field_alias=alias)
+        arcpy.AddMessage('    Alias set on {0}'.format(field))
+    arcpy.AddMessage('Aliases Set')
+arcpy.AddMessage('Complete: Create FMG Public View')
+
+# Set output parameters from out_feature_classes list
+if len(out_feature_classes) == 4:
+    arcpy.SetParameterAsText(10, out_feature_classes[0])
+    arcpy.SetParameterAsText(11, out_feature_classes[1])
+    arcpy.SetParameterAsText(12, out_feature_classes[2])
+    arcpy.SetParameterAsText(13, out_feature_classes[3])
+
+elif len(out_feature_classes) == 3:
+    arcpy.SetParameterAsText(10, out_feature_classes[0])
+    arcpy.SetParameterAsText(11, out_feature_classes[1])
+    arcpy.SetParameterAsText(12, out_feature_classes[2])
+
+elif len(out_feature_classes) == 2:
+    arcpy.SetParameterAsText(10, out_feature_classes[0])
+    arcpy.SetParameterAsText(11, out_feature_classes[1])
+
+elif len(out_feature_classes) == 1:
+    arcpy.SetParameterAsText(10, out_feature_classes[0])
 
 
 
