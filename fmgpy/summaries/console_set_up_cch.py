e9e26d1995eb309e54583329c8a0f9b5a112e305
Chris Hawes <chris.hawes9@gmail.com>
2024-09-23 14:39:22 -0500

    little update of console set up

--- fmgpy/summaries/console_set_up_cch.py
+++ fmgpy/summaries/console_set_up_cch.py
@@ -10,7 +10,7 @@ import pandas as pd
 import numpy as np
 import itertools
 import importlib
-import summaries.forest_calcs as fcalc
+import fmgpy.summaries.forest_calcs as fcalc
 
 
 # Define Some input data - pool 12 full
