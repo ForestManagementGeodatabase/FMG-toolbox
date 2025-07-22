c32b90a81fdbd0935b0d36c58029808bfeb2b3d8
STEELE <isaiah.w.steele@gmail.com>
2025-07-22 15:13:11 -0500

    Correct and condense test_create_plot_table

--- fmgpy/test/test_fmglib/test_forest_calcs/test_create_plot_table.py
+++ fmgpy/test/test_fmglib/test_forest_calcs/test_create_plot_table.py
@@ -2,66 +2,36 @@ import fmgpy.fmglib.forest_calcs as fcalc
 import pandas as pd
 
 
-def test_size():
+def test_dataframe_size():
     # define test data location
     age = r'C:\Users\b5ecdiws\Documents\FMG\fmg_test_data\FMG_FieldData_QA_20250513\FMG_FieldData_QA_20250513.gdb\Age_QA_20250513'
     fixed = r'C:\Users\b5ecdiws\Documents\FMG\fmg_test_data\FMG_FieldData_QA_20250513\FMG_FieldData_QA_20250513.gdb\Fixed_QA_20250513'
 
-    # create age, fixed, and prism dataframes from test data
     age_df = pd.DataFrame.spatial.from_featureclass(age)
     fixed_df = pd.DataFrame.spatial.from_featureclass(fixed)
 
-    # create tree and plot tables from age, fixed, and prism dataframes
     plot_table = fcalc.create_plot_table(fixed_df, age_df)
 
     assert plot_table.shape[0] == fixed_df.shape[0] # test number of rows
 
-
-def test_column_locations():
-    # define test data location
+def test_column_names():
     age = r'C:\Users\b5ecdiws\Documents\FMG\fmg_test_data\FMG_FieldData_QA_20250513\FMG_FieldData_QA_20250513.gdb\Age_QA_20250513'
     fixed = r'C:\Users\b5ecdiws\Documents\FMG\fmg_test_data\FMG_FieldData_QA_20250513\FMG_FieldData_QA_20250513.gdb\Fixed_QA_20250513'
 
-    # create age, fixed, and prism dataframes from test data
     age_df = pd.DataFrame.spatial.from_featureclass(age)
     fixed_df = pd.DataFrame.spatial.from_featureclass(fixed)
 
-    # create tree and plot tables from age, fixed, and prism dataframes
     plot_table = fcalc.create_plot_table(fixed_df, age_df)
 
-    # PID column
-    plot_subset_1 = plot_table.iloc[:, 0:1]
-    fixed_subset_1 = fixed_df.iloc[:, 35:36]
+    cols_to_check = ['PID', 'PLOT', 'OV_CLSR', 'OV_HT', 'UND_HT', 'UND_COV', 'UND_SP1', 'UND_SP2', 'UND_SP3', 'GRD_SP1',
+                     'GRD_SP2', 'GRD_SP3', 'NOT_SP1', 'NOT_SP2', 'NOT_SP3', 'NOT_SP4', 'NOT_SP5', 'FX_MISC', 'COL_CREW',
+                     'COL_DATE', 'FP_TIME', 'MIS_FIELDS', 'HAS_MIS_FIELD', 'UND_HT2', 'VALID_PLOT_ID',
+                     'METERS_FROM_PLOT_CENTER', 'CORRECT_PLOT_ID', 'HAS_PRISM', 'DUPLICATE', 'PRISM_ID_MATCHES', 'POOL',
+                     'COMP', 'UNIT', 'SITE', 'SID', 'VALID_SID', 'SHAPE', 'AGE_SP', 'AGE_DIA', 'AGE_ORIG', 'AGE_GRW',
+                     'AGE_MISC', 'MAST_TYPE', 'INV_SP', 'INV_PRESENT']
 
-    # FX_MISC / MISC column
-    plot_subset_3 = plot_table.iloc[:, 1:3]
-    fixed_subset_3 = fixed_df.iloc[:, 1:3]
-
-    # Plot through NOT_SP5
-    plot_subset_2 = plot_table.iloc[:, 1:17]
-    fixed_subset_2 = fixed_df.iloc[:, 1:17]
-
-    # COL_CREW through SID
-    plot_subset_4 = plot_table.iloc[:, 18:35]
-    fixed_subset_4 = fixed_df.iloc[:, 18:35]
-
-    # VALID_SID through SHAPE
-    plot_subset_5 = plot_table.iloc[:, 35:37]
-    fixed_subset_5 = fixed_df.iloc[:, 36:38]
-
-    # check for equal data
-    pd.testing.assert_frame_equal(plot_subset_1, fixed_subset_1)
-    pd.testing.assert_frame_equal(plot_subset_2, fixed_subset_2)
-    assert plot_subset_3.equals(fixed_subset_3)
-    pd.testing.assert_frame_equal(plot_subset_4, fixed_subset_4)
-    pd.testing.assert_frame_equal(plot_subset_5, fixed_subset_5)
-
-    # Check for age columns
-    age_columns = ["POOL", "COMP", "UNIT", "SITE", "SID", "VALID_SID", "SHAPE", "AGE_SP", "AGE_DIA", "AGE_ORIG", "AGE_GRW", "AGE_MISC", "MAST_TYPE"]
-    for col in age_columns:
-        assert col in plot_table.columns
-
-
+    assert set(cols_to_check).issubset(plot_table.columns)
+    assert len(cols_to_check) == len(plot_table.columns)
 
 def test_data_types():
     # define test data location
@@ -75,52 +45,22 @@ def test_data_types():
     # create tree and plot tables from age, fixed, and prism dataframes
     plot_table = fcalc.create_plot_table(fixed_df, age_df)
 
-    output_data_types = pd.Series({
-        'PID': 'string[python]',
-        'PLOT': 'Int32',
-        'OV_CLSR': 'string[python]',
-        'OV_HT': 'string[python]',
-        'UND_HT': 'string[python]',
-        'UND_COV': 'string[python]',
-        'UND_SP1': 'string[python]',
-        'UND_SP2': 'string[python]',
-        'UND_SP3': 'string[python]',
-        'GRD_SP1': 'string[python]',
-        'GRD_SP2': 'string[python]',
-        'GRD_SP3': 'string[python]',
-        'NOT_SP1': 'string[python]',
-        'NOT_SP2': 'string[python]',
-        'NOT_SP3': 'string[python]',
-        'NOT_SP4': 'string[python]',
-        'NOT_SP5': 'string[python]',
-        'FX_MISC': 'string[python]',
-        'COL_CREW': 'string[python]',
-        'COL_DATE': 'datetime64[us]',
-        'FP_TIME': 'string[python]',
-        'MIS_FIELDS': 'string[python]',
-        'HAS_MIS_FIELD': 'string[python]',
-        'UND_HT2': 'Float64',
-        'VALID_PLOT_ID': 'string[python]',
-        'METERS_FROM_PLOT_CENTER': 'Float64',
-        'CORRECT_PLOT_ID': 'string[python]',
-        'HAS_PRISM': 'string[python]',
-        'DUPLICATE': 'string[python]',
-        'PRISM_ID_MATCHES': 'string[python]',
-        'POOL': 'string[python]',
-        'COMP': 'string[python]',
-        'UNIT': 'string[python]',
-        'SITE': 'string[python]',
-        'SID': 'string[python]',
-        'VALID_SID': 'string[python]',
-        'SHAPE': 'geometry',
-        'AGE_SP': 'string[python]',
-        'AGE_DIA': 'Float64',
-        'AGE_ORIG': 'Int32',
-        'AGE_GRW': 'Int32',
-        'AGE_MISC': 'string[python]',
-        'MAST_TYPE': 'string[python]',
-        'INV_SP': 'string[python]',
-        'INV_PRESENT': 'string[python]'
+    output_dtypes = pd.Series({
+        'PID': 'string[python]', 'PLOT': 'Int32', 'OV_CLSR': 'string[python]', 'OV_HT': 'string[python]',
+        'UND_HT': 'string[python]', 'UND_COV': 'string[python]', 'UND_SP1': 'string[python]',
+        'UND_SP2': 'string[python]', 'UND_SP3': 'string[python]', 'GRD_SP1': 'string[python]',
+        'GRD_SP2': 'string[python]', 'GRD_SP3': 'string[python]', 'NOT_SP1': 'string[python]',
+        'NOT_SP2': 'string[python]', 'NOT_SP3': 'string[python]', 'NOT_SP4': 'string[python]',
+        'NOT_SP5': 'string[python]', 'FX_MISC': 'string[python]', 'COL_CREW': 'string[python]',
+        'COL_DATE': 'datetime64[us]', 'FP_TIME': 'string[python]', 'MIS_FIELDS': 'string[python]',
+        'HAS_MIS_FIELD': 'string[python]', 'UND_HT2': 'Float64', 'VALID_PLOT_ID': 'string[python]',
+        'METERS_FROM_PLOT_CENTER': 'Float64', 'CORRECT_PLOT_ID': 'string[python]', 'HAS_PRISM': 'string[python]',
+        'DUPLICATE': 'string[python]', 'PRISM_ID_MATCHES': 'string[python]', 'POOL': 'string[python]',
+        'COMP': 'string[python]', 'UNIT': 'string[python]', 'SITE': 'string[python]', 'SID': 'string[python]',
+        'VALID_SID': 'string[python]', 'SHAPE': 'geometry', 'AGE_SP': 'string[python]', 'AGE_DIA': 'Float64',
+        'AGE_ORIG': 'Int32', 'AGE_GRW': 'Int32', 'AGE_MISC': 'string[python]', 'MAST_TYPE': 'string[python]',
+        'INV_SP': 'string[python]', 'INV_PRESENT': 'string[python]'
     })
+    plot_table_dtypes = plot_table.dtypes
 
-    assert plot_table.dtypes.equals(output_data_types)
+    assert (output_dtypes.loc[plot_table_dtypes.index] == plot_table_dtypes).all()
