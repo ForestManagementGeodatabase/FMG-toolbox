214615ee89a8d39e26972a91ab5e97b9d966a83c
Steele <isaiah.w.steele@gmail.com>
2025-08-04 09:37:23 -0500

    Write initial tests for health_dom_level

---
+++ fmgpy/test/test_fmglib/test_forest_calcs/test_health_dom_level.py
@@ -0,0 +1,42 @@
+import fmgpy.fmglib.forest_calcs as fcalc
+import pandas as pd
+
+def test_dataframe_size():
+    prism = r'C:\Users\b5ecdiws\Documents\FMG\fmg_test_data\FMG_FieldData_QA_20250513\FMG_FieldData_QA_20250513.gdb\Prism_QA_20250513'
+    filter_statement = None
+    level = 'SID'
+
+    prism_df = pd.DataFrame.spatial.from_featureclass(prism)
+    tree_table = fcalc.create_tree_table(prism_df)
+    hdl_table = fcalc.health_dom_level(tree_table, filter_statement, level)
+
+    #TODO: Write test for the correct number of rows. This could be converted to an end-to-end-test of the function,
+    # testing the entire dataframe after several different filter statements.
+def test_column_existence():
+    prism = r'C:\Users\b5ecdiws\Documents\FMG\fmg_test_data\FMG_FieldData_QA_20250513\FMG_FieldData_QA_20250513.gdb\Prism_QA_20250513'
+    filter_statement = None
+    level = 'SID'
+
+    prism_df = pd.DataFrame.spatial.from_featureclass(prism)
+    tree_table = fcalc.create_tree_table(prism_df)
+    hdl_table = fcalc.health_dom_level(tree_table, filter_statement, level)
+
+    asserted_columns = ['SID', 'HLTH_DOM', 'HLTH_DOM_PCMP']
+
+    assert set(asserted_columns).issubset(hdl_table.columns)
+    assert len(asserted_columns) == len(hdl_table.columns)
+
+def test_data_types():
+    prism = r'C:\Users\b5ecdiws\Documents\FMG\fmg_test_data\FMG_FieldData_QA_20250513\FMG_FieldData_QA_20250513.gdb\Prism_QA_20250513'
+    filter_statement = None
+    level = 'SID'
+
+    prism_df = pd.DataFrame.spatial.from_featureclass(prism)
+    tree_table = fcalc.create_tree_table(prism_df)
+    hdl_table = fcalc.health_dom_level(tree_table, filter_statement, level)
+
+    asserted_dtypes = pd.Series({
+        'SID': 'string[python]', 'HLTH_DOM': 'string[python]', 'HLTH_DOM_PCMP': 'float64'
+    })
+
+    assert (asserted_dtypes.loc[hdl_table.dtypes.index] == hdl_table.dtypes).all()
