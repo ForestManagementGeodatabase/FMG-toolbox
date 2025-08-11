5bee227cf064fcde757dcc2a658e1b622e48317f
Steele <isaiah.w.steele@gmail.com>
2025-08-11 13:42:13 -0500

    Test health_dom_level against itself.

--- fmgpy/test/test_fmglib/test_forest_calcs/test_health_dom_level.py
+++ fmgpy/test/test_fmglib/test_forest_calcs/test_health_dom_level.py
@@ -1,5 +1,58 @@
 import fmgpy.fmglib.forest_calcs as fcalc
 import pandas as pd
+import pandas.testing as pdt
+
+
+def test_itself():
+    prism = r'C:\Users\b5ecdiws\Documents\FMG\fmg_test_data\FMG_FieldData_QA_20250513\FMG_FieldData_QA_20250513.gdb\Prism_QA_20250513'
+    prism_df = pd.DataFrame.spatial.from_featureclass(prism)
+    tree_table = fcalc.create_tree_table(prism_df)
+
+    csv_folder_path = './dataframe_test_csvs/health_dom_level/'
+    none_pid = csv_folder_path + 'none_pid.csv'
+    mastType_hard_pool = csv_folder_path + 'mastType_hard_pool.csv'
+
+    filter_statement = None
+    health_dom_level_none_pid = fcalc.tpa_ba_qmdbh_level(tree_table, filter_statement, 'SID')
+    #health_dom_level_none_pid.to_csv('./dataframe_test_csvs/health_dom_level/none_pid.csv', index=False)
+
+    filter_statement = tree_table.MAST_TYPE == 'Hard'
+    health_dom_level_mastType_hard_pool = fcalc.tpa_ba_qmdbh_level(tree_table, filter_statement, 'POOL')
+    #health_dom_level_mastType_hard_pool.to_csv('./dataframe_test_csvs/health_dom_level/mastType_hard_pool.csv', index=False)
+
+    asserted_dataframe_none_pid = pd.read_csv(none_pid)
+    asserted_dataframe_mastType_hard_pool = pd.read_csv(mastType_hard_pool)
+
+    health_dom_level_none_pid = health_dom_level_none_pid.replace('', pd.NA)
+    asserted_dataframe_none_pid = asserted_dataframe_none_pid.replace('', pd.NA)
+
+    health_dom_level_mastType_hard_pool = health_dom_level_mastType_hard_pool.replace('', pd.NA)
+    asserted_dataframe_mastType_hard_pool = asserted_dataframe_mastType_hard_pool.replace('', pd.NA)
+
+    asserted_dataframe_none_pid = asserted_dataframe_none_pid.astype({
+        'index': 'int64',
+        'SID': 'string[python]',
+        'tree_count': 'float64',
+        'stand_dens': 'float64',
+        'plot_count': 'float64',
+        'TPA': 'float64',
+        'BA': 'float64',
+        'QM_DBH': 'float64'
+    })
+    asserted_dataframe_mastType_hard_pool = asserted_dataframe_mastType_hard_pool.astype({
+        'index': 'int64',
+        'POOL': 'string[python]',
+        'tree_count': 'float64',
+        'stand_dens': 'float64',
+        'plot_count': 'float64',
+        'TPA': 'float64',
+        'BA': 'float64',
+        'QM_DBH': 'float64'
+    })
+
+    pdt.assert_frame_equal(health_dom_level_none_pid, asserted_dataframe_none_pid)
+    pdt.assert_frame_equal(health_dom_level_mastType_hard_pool, asserted_dataframe_mastType_hard_pool)
+
 
 def test_dataframe_size():
     prism = r'C:\Users\b5ecdiws\Documents\FMG\fmg_test_data\FMG_FieldData_QA_20250513\FMG_FieldData_QA_20250513.gdb\Prism_QA_20250513'
